3730bc3fdd701d8108198de26f8f4262495bc692
duyngudao <ddngu0110@gmail.com>
2022-11-03 14:06:31 +0700

    version-1.0

--- classification_stgcn/evaluate_STGCN.py
+++ classification_stgcn/evaluate_STGCN.py
@@ -59,7 +59,7 @@ def plot_cm(CM, normalize=True, save_dir='', names_x=(), names_y=(), show=True):
 
 if __name__ == "__main__":
 
-    path_test = '/home/duyngu/Downloads/Dataset_Human_Action/test_no_scale.pkl'
+    path_test = '/home/duyngu/Downloads/test_no_scale.pkl'
     batch_size = 32
     # Load dataset
     features, labels = [], []
